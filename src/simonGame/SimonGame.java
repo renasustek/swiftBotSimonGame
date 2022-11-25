6d73cdce4a94b60cffb7229d00675cf98e293c4c
renasustek <renasustek@gmail.com>
2022-11-25 22:19:26 +0000

    cant remeber last time i commited, will continue from now on. trying to get user inputs (via buttons on the swiftbot)

--- src/simonGame/SimonGame.java
+++ src/simonGame/SimonGame.java
@@ -1,8 +1,11 @@
 package simonGame;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.util.Console;
 
 import swiftbot.SwiftBotAPI;
@@ -16,6 +19,7 @@ public class SimonGame {
 
 
         swiftBot = new SwiftBotAPI();
+        swiftBot.fillButtonLights();
         final Console console = new Console();
         console.title("PLAY ");
         Scanner reader = new Scanner(System.in);
@@ -47,43 +51,148 @@ public class SimonGame {
 
     private static void play() throws InterruptedException, IOException {
         System.out.println("playyyy timmmmee");
-        int counter = 6;
+        int counter = 2;
         int lives = 3;
         //while (lives!=0){
         int[] colourArray = new int[counter];
 
         addColoursToArray(colourArray);
-
+        userInputAndCheck(colourArray);
         ///}
 
     }
 
-    private static void displayingColoursInArray(int[] colourArray) throws IOException, InterruptedException {
-        System.out.println("displayingColoursInArray");
-        for (int j : colourArray) {
-            if (j == 0) {
-                swiftBot.setUnderlight(SwiftBotAPI.Underlight.FRONT_LEFT, 255, 0, 0);
-                Thread.sleep(2000);
-                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.FRONT_LEFT,false);
-                swiftBot.updateUnderlights();
-            } else if (j == 1) {
-                swiftBot.setUnderlight(SwiftBotAPI.Underlight.FRONT_RIGHT, 0, 255, 0);
-                Thread.sleep(2000);
-                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.FRONT_RIGHT,false);
-                swiftBot.updateUnderlights();
-            } else if (j == 2) {
-                swiftBot.setUnderlight(SwiftBotAPI.Underlight.BACK_LEFT, 0, 0, 255);
-                Thread.sleep(2000);
-                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.BACK_LEFT,false);
-                swiftBot.updateUnderlights();
-            } else if (j == 3) {
-                swiftBot.setUnderlight(SwiftBotAPI.Underlight.BACK_RIGHT, 255, 255, 0);
-                Thread.sleep(2000);
-                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.BACK_RIGHT,false);
-                swiftBot.updateUnderlights();
+    private static void userInputAndCheck(int[] colourArray) {
+        System.out.println("user input and check");
+        final boolean[] buttonPressed = new boolean[4];
+        swiftBot.BUTTON_A.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState().isLow()) {
+                    buttonPressed[0] = true;
+                }
             }
+        });
+        swiftBot.BUTTON_X.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState().isLow()) {
+                    buttonPressed[1] = true;
+                }
+            }
+        });
+        swiftBot.BUTTON_B.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState().isLow()) {
+                    buttonPressed[2] = true;
+                }
+            }
+        });
+        swiftBot.BUTTON_Y.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState().isLow()) {
+                    buttonPressed[3] = true;
+                }
+            }
+        });
+
+        for (int x: colourArray){
         }
     }
+
+
+        /* for (int x : colourArray) {
+
+                    System.out.println("A PRESSED");
+                    try {
+                        displayColourSimplifiyer(255, 0, 0, "FL", 750);
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (x != 0) {
+                        boolean y = false;
+                    }
+                }
+
+            }
+        });
+
+        swiftBot.BUTTON_A.removeAllListeners();
+        swiftBot.BUTTON_X.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+
+                if (event.getState().isLow()) {
+
+                    try {
+                        displayColourSimplifiyer(0, 255, 0, "FR", 750);
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (x != 1) {
+                        boolean y = false;
+                    }
+                }
+
+            }
+        });
+        swiftBot.BUTTON_X.removeAllListeners();
+        swiftBot.BUTTON_B.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+
+                if (event.getState().isLow()) {
+
+                    try {
+                        displayColourSimplifiyer(0, 0, 255, "BR", 750);
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (x != 2) {
+                        boolean y = false;
+                    }
+                }
+
+            }
+        });
+        swiftBot.BUTTON_B.removeAllListeners();
+        swiftBot.BUTTON_Y.addListener(new GpioPinListenerDigital() {
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+
+                if (event.getState().isLow()) {
+
+                    try {
+                        displayColourSimplifiyer(255, 255, 0, "BL", 750);
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (x != 3) {
+                        boolean y = false;
+                    }
+                }
+
+            }
+        });
+        swiftBot.BUTTON_Y.removeAllListeners();
+        /////}
+    }
+*/
+
+        private static void displayingColoursInArray ( int[] colourArray) throws IOException, InterruptedException {
+            System.out.println("displayingColoursInArray");
+            for (int j : colourArray) {
+                if (j == 0) {
+                    displayColourSimplifiyer(255, 0, 0, "FL", 1000);
+                } else if (j == 1) {
+                    displayColourSimplifiyer(0, 255, 0, "FR", 1000);
+                } else if (j == 2) {
+                    displayColourSimplifiyer(0, 0, 255, "BL", 1000);
+                } else if (j == 3) {
+                    displayColourSimplifiyer(255, 255, 0, "BR", 1000);
+
+                }
+            }
+        }
 
 
         private static void addColoursToArray ( int[] colourArray) throws IOException, InterruptedException {
@@ -96,5 +205,46 @@ public class SimonGame {
             }
             displayingColoursInArray(colourArray);
         }
+
+        public static void displayColourSimplifiyer ( int r, int g, int b, String WhichLight,int time) throws
+        IOException, InterruptedException {
+            if (Objects.equals(WhichLight, "FL")) {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.FRONT_LEFT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.FRONT_LEFT, false);
+                swiftBot.updateUnderlights();
+            } else if (Objects.equals(WhichLight, "FR")) {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.FRONT_RIGHT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.FRONT_RIGHT, false);
+                swiftBot.updateUnderlights();
+            } else if (Objects.equals(WhichLight, "ML")) {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.MIDDLE_LEFT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.MIDDLE_LEFT, false);
+                swiftBot.updateUnderlights();
+            } else if (Objects.equals(WhichLight, "MR")) {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.MIDDLE_RIGHT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.MIDDLE_RIGHT, false);
+                swiftBot.updateUnderlights();
+            } else if (Objects.equals(WhichLight, "BL")) {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.BACK_LEFT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.BACK_LEFT, false);
+                swiftBot.updateUnderlights();
+            } else if (WhichLight == "BR") {
+                swiftBot.setUnderlight(SwiftBotAPI.Underlight.BACK_RIGHT, r, g, b);
+                Thread.sleep(time);
+                swiftBot.disableUnderlight(SwiftBotAPI.Underlight.BACK_RIGHT, false);
+                swiftBot.updateUnderlights();
+            } else {
+                System.out.println("Invalid options");
+            }
+
+        }
     }
+
+
+
 
